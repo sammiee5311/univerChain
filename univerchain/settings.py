6e9598909984a49f1933cd4613007f946322145c
sammiee5311 <56215891+sammiee5311@users.noreply.github.com>
2021-06-13 22:57:03 +0900

    web3.py update
    - connected web3.py

--- univerchain/settings.py
+++ univerchain/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'ether.apps.EtherConfig',
     'pages.apps.PagesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -121,9 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, '/')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'univerchain/static')
+    os.path.join(BASE_DIR, 'static/')
 ]
 
 # Default primary key field type
