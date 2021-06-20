e9ded61be214b1920a5a5e02cb1c2aa55e228f15
sammiee5311 <56215891+sammiee5311@users.noreply.github.com>
2021-06-20 23:06:45 +0900

    ether update
    - changed ether page

--- ether/ethereum.py
+++ ether/ethereum.py
@@ -9,7 +9,7 @@ class Ethereum:
         self.ip = 'localhost' # default ip
         self.port = '8545' # default port
         self.filename = './files/abi.yml' # default abi file
-        self.SMART_CONTRACT_ADDRESS = '0x1d555a31a5b5b5619AF474901d3c8870343B9e99'
+        self.SMART_CONTRACT_ADDRESS = '0x575fc0F0916aCd9E3CB6Ed0DAd5E952cc0636075'
         self.SMART_CONTTRACT_ABI = self.get_abi()
         self.web3 = self.connect_to_web3()
         self.__contract = self.get_contract()
--- ether/views.py
+++ ether/views.py
@@ -1,26 +1,33 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .ethereum import Ethereum
 
 ethereum = Ethereum()
 
 def check(request):
-    say = None
+    registered = None
    
     if request.method == 'POST':
         account = request.POST['etherAccount']
         contract = ethereum.Contract
         ether_state = True if contract else False
             
-        if ether_state and 'greeting' in request.POST:
-            greeting = request.POST['greeting']
-            contract.functions.setGreeting(greeting).transact({'from': account})
-        elif ether_state and 'say' in request.POST:
-            say = contract.functions.say().call()
+        if ether_state and 'type_name' in request.POST:
+            type_name = request.POST['type_name']
+            grade = request.POST['grade']
+            try:
+                contract.functions.register(type_name, int(grade)).transact({'from': account})
+            except:
+                messages.error(request, 'User already existed.')
+                
+        elif ether_state and 'registered' in request.POST:
+            registered = contract.functions.registeredStudent(account).call({'from': account})
+            registered = 'currently registered' if registered else 'you are not registered.'
         else:
             """ error message """
 
     context = {
-        'say' : say
+        'registered' : registered
     }
 
     return render(request, 'ether/main.html', context)
